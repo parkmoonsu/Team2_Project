02d6038e4a0387da77ff70fc7263d3786e929e59
박문수 <overecho@naver.com>
2016-11-17 16:32:10 +0900

    충돌좀잡자

---
+++ WebContent/WEB-INF/views/login/searchidfail.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+
+</head>
+<body>
+<script type="text/javascript">
+	alert("입력된 정보로 가입된 ID가 없습니다.");
+	location.href = "search.htm";
+</script>
+</body>
+</html>
--- src/kr/or/bus/controller/LoginController.java
+++ src/kr/or/bus/controller/LoginController.java
@@ -8,11 +8,17 @@
 
 package kr.or.bus.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.bus.service.LoginService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private LoginService service;
 	
 	@RequestMapping("/login.htm")
 	public String login(){
@@ -22,5 +28,36 @@ public class LoginController {
 	@RequestMapping("/main.htm")
 	public String mainGo(){
 		return "main/main";
+	}
+	
+	@RequestMapping("/search.htm")
+	public String search(){
+		return "login/search";
+	}
+	
+	@RequestMapping("/searchid.htm")
+	public String searchId(String m_name , String m_email) throws Exception{
+		String viewpage = "";
+		
+		if(service.searchName(m_name, m_email).equals("")){
+			viewpage = "login/searchidfail";
+		}else{
+			viewpage = "login/searchidsuccess";
+		}
+		
+		return viewpage;
+	}
+	
+	@RequestMapping("/searchpw.htm")
+	public String searchPw(String m_id , String m_email) throws Exception{
+		String viewpage = "";
+		
+		if(service.searchPw(m_id, m_email).equals("")){
+			viewpage = "login/searchidfail";
+		}else{
+			viewpage = "login/searchpwsuccess";
+		}
+		
+		return viewpage;
 	}
 }
