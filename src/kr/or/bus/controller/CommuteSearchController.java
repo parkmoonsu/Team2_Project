a2e7432d4a9d1d47e96be4fa01a1e78ba5dbec41
조한솔 <chohansol123@naver.com>
2016-11-21 23:47:33 +0900

    ㅎㅅㅎ

--- src/kr/or/bus/controller/CommuteSearchController.java
+++ src/kr/or/bus/controller/CommuteSearchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.or.bus.dao.CommuteDAO;
-import kr.or.bus.dto.CommuteDTO;
+import kr.or.bus.dto.CommuteJoinCstartJoinCendDTO;
 
 
 @Controller
@@ -20,8 +20,10 @@ public class CommuteSearchController {
 	private SqlSession sqlsession;
 	
 	@RequestMapping(value="/comsearch.htm", method=RequestMethod.GET)
-	public String csearch(CommuteDTO m_id, Model model){
+	//public String csearch(CommuteDTO m_id, Model model){
 	//public String csearch(String pg , String f , String q , CommuteDTO m_id, Model model){
+	public String csearch(String pg , String q , CommuteJoinCstartJoinCendDTO m_id, Model model){
+		
 		System.out.println("들어왔으??");
 		System.out.println("m_id" + m_id);
 		String id = m_id.getM_id();
@@ -29,26 +31,24 @@ public class CommuteSearchController {
 		
 		//System.out.println("m_id????"+comdao.getSelect(m_id));
 		////
-		/*int page = 1;
-		String field = "TITLE";
+		int page = 1;
+		//String field = "C_DATE";
 		String query ="%%";
 		
 		if(pg != null && pg.equals("")){
 			page = Integer.parseInt(pg);
 		}
-		if(f != null && f.equals("")){
+		/*if(f != null && f.equals("")){
 			field = f;
-		}
+		}*/
 		if(q != null && q.equals("")){
 			query = q;
-		}*/
-		
-		//System.out.println(page + " / " + field + " / " + query);
-		
-		
+		}
+			
 		CommuteDAO  comdao = sqlsession.getMapper(CommuteDAO.class);
-		//List<CommuteDTO> list= comdao.getSelect(page, field, query,id);	
-		List<CommuteDTO> list = comdao.getSelect(id);
+		List<CommuteJoinCstartJoinCendDTO> list= comdao.getSelect(page, query, id);	
+		//List<CommuteDTO> list= comdao.getSelect(page, field, query, id);
+		//List<CommuteDTO> list = comdao.getSelect(id);
 		
 		for(int i=0; i<list.size(); i++){	
 			System.out.println("asdfsaf" +list.get(i).getC_start());
@@ -57,10 +57,37 @@ public class CommuteSearchController {
 		}
 		
 		System.out.println("ddd" + list);
-		
-		
 		model.addAttribute("list",list);
 		
-		return "main/comsearch";	
+		return "commute/comsearch";	
 	}
+
+	@RequestMapping(value="/gotowork.htm", method=RequestMethod.GET)
+	public String show(){
+		System.out.println("이거는 들어왔냐고~~");
+		
+			
+		return "commute/gotowork";
+	}
+	
+	@RequestMapping(value="/show.htm", method=RequestMethod.POST)
+	public String go(){
+		System.out.println("이거는~");
+		
+		CommuteDAO  comdao = sqlsession.getMapper(CommuteDAO.class);
+		CommuteJoinCstartJoinCendDTO dto = new CommuteJoinCstartJoinCendDTO();
+		CommuteJoinCstartJoinCendDTO cjdto = comdao.csupdate(dto);
+		cjdto=csselect();
+		model.addAttribute("cjdto",cjdto);
+			
+		return "commute/gotowork";
+	}
+	
+	
+	/*@RequestMapping(value="/getoffwork.htm")
+	public String get(){
+		
+		return "commute/getoffwork";
+		
+	}*/
 }
