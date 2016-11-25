ccf5be9f4ff10bbc26bf4c49bbb828b559b88727
KimJihyeon <ok2103ing@naver.com>
2016-11-26 03:57:04 +0900

    버스정류장관리 컨트롤러 , 서비스 구현 완료 및 버스정류장 뷰 생성

--- src/kr/or/bus/controller/BusStopManageController.java
+++ src/kr/or/bus/controller/BusStopManageController.java
@@ -1,7 +1,12 @@
 package kr.or.bus.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.or.bus.service.BusStopManageService;
 
@@ -9,4 +14,22 @@ import kr.or.bus.service.BusStopManageService;
 public class BusStopManageController {
 	@Autowired
 	BusStopManageService busStopManageService;
+	
+	//수정된 버스정류장 마커 좌표를 저장한다.
+	@RequestMapping(value="/busStoplocationEdit.admin",method=RequestMethod.GET)
+	public void busStopEditSave(HttpServletRequest request, HttpServletResponse response ) throws Exception{
+		busStopManageService.busStoplocationEdit(request, response);
+	}
+		
+	//수정된 버스정류장 좌표를 파일에서 읽어온다.
+	@RequestMapping(value="/busStoplocationRead.admin",method=RequestMethod.GET)
+	public void busStopEditRead(HttpServletRequest request ,HttpServletResponse response) throws Exception{
+		busStopManageService.busStoplocationEditRead(request, response);
+	}
+		
+	//버스정류장 원본 마커 좌표를 파일에서 읽어온다.
+	@RequestMapping(value="/busStopOriginalRead.admin",method=RequestMethod.GET)
+	public void busStopOriginRead(HttpServletRequest request ,HttpServletResponse response) throws Exception{
+		busStopManageService.busStopOriginalRead(request, response);
+	}
 }
