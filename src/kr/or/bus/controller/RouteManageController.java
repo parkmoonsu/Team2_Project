c092ccd38f525650c237c711fd29a9c53b2daedd
KimJihyeon <ok2103ing@naver.com>
2016-11-26 00:36:39 +0900

    노선관리컨트롤러 , 노선관리서비스 class 추가합니다.

---
+++ src/kr/or/bus/controller/RouteManageController.java
@@ -0,0 +1,8 @@
+package kr.or.bus.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RouteManageController {
+
+}
---
+++ src/kr/or/bus/service/RouteManageService.java
@@ -0,0 +1,8 @@
+package kr.or.bus.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteManageService {
+
+}
